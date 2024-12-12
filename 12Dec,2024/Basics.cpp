fe38b9d4ce2ec08f4d1fb7c03c4b4de099059d2f
Ankush-asabharwal <ankusha0209.be21@gmail.com>
2024-12-12 21:08:45 +0530

    adding files

---
+++ 12Dec,2024/Basics.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+int main(){
+    // concatenate
+    // string firstName = "Ankusha";
+    // string lastName = "Sabharwal";
+    // string fullName = firstName + " " + lastName;
+    // cout << fullName;
+
+
+    //Append
+    // string firstName = "Ankusha";
+    // string lastName = "Sabharwal";
+    // string fullName = firstName.append(lastName);
+    // cout << fullName;
+
+
+    // Length and size is used for finding the length of the text
+    // string txt = "ankushaSabharwal";
+    // cout << "the length of the string is " << txt.length();
+    //using size() function
+    // string txt = "ankushaSabharwal";
+    // cout << "the length of the string is " << txt.size();
+
+
+    // to acess the first and last Character of the string
+    // string txt = "Ankusha Sabharwal";
+    // cout << txt[0] << "\n";
+    // cout << txt[txt.length()-1];
+
+
+    //Change the String characters
+    // string txt = "Naman";
+    // txt[0] = 'D';
+    // cout << txt;
+
+
+    //.at() function is also used to acess any character in string
+    // string txt = "Ankusha";
+    // cout << txt.at(0) << "\n";
+    // cout << txt.at(1) << "\n";
+    // cout << txt.at(txt.length()-1) << " \n";
+    // txt[0] = 'Z';
+    // cout << txt << "\n";
+
+
+    //.getLine() function read the line for the input
+    // string txt;
+    // cout << "Enter full name : " ;
+    // getline(cin, txt);
+    // cout << "Your full name is : ";
+    // cout << txt;
+
+
+    //Maths
+    //to find max and min between two numbers
+    // cout << max(20,50) << "\n";
+    // cout << min(60,67); 
+    // to find the sqrt, round, and log of the number
+    // cout << "The square root of 64 is : " << sqrt(64) << "\n";
+    // cout << "2.66 is round of to : " << round(2.66) << "\n";
+    // cout << "The log of 2 is : " << log(2) << "\n";
+
+
+    // Write a program that enter the age is adult or not
+    // int age;
+    // cin >> age;
+    // int adultAge = 18;
+    // if(age >= adultAge){
+    //     cout << "You are adult";
+    // }
+    // else cout << "Not adult";
+
+
+    //Write a program of students to give grades 
+    // int marks;
+    // cin >> marks;
+    // if(marks<=25){
+    //     cout << 'F';
+    // }else if(marks>25 && marks <= 44){
+    //     cout << 'E';
+    // }else if(marks>=45 && marks<=49){
+    //     cout << 'D';
+    // }else if(marks>=50 && marks<=59){
+    //     cout << 'C';
+    // }else if(marks>=60 && marks<=79){
+    //     cout << 'B';
+    // }else if(marks>=80 && marks<=100){
+    //     cout << 'A';
+    // }
+    // return 0;
+
+
+    //Write a program to write accordingly that person is eligible for job or not, retirement time soon and retirement
+    // int age;
+    // cin >> age;
+    // if(age<18){
+    //     cout << "Not Eligible for Job";
+    // }else if(age>=18 && age<55){
+    //     cout << "Eligible for Job";
+    // }else if(age>=55 && age<=57){
+    //     cout << "Eligible for Job but retirement soon";
+    // }else if(age>57){
+    //     cout << "Retirement time";
+    // }
+    // return 0;
+
+    //Print days by using print number using switch-case statement
+    // int day;
+    // cin >> day;
+    // switch (day){
+    //     case 1: 
+    //     cout << "Monday";
+    //     break;
+    //     case 2: 
+    //     cout << "Tuesday";
+    //     break;
+    //     case 3: 
+    //     cout << "Wednesday";
+    //     break;
+    //     case 4: 
+    //     cout << "Thursday";
+    //     break;
+    //     case 5: 
+    //     cout << "Friday";
+    //     break;
+    //     case 6: 
+    //     cout << "Saturday";
+    //     break;
+    //     case 7: 
+    //     cout << "Sunday";
+    //     break;
+    // }
+}
