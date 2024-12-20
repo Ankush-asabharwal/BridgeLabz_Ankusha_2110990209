36f4d3e1ba450f1cf6620a17a22806be764d319e
Ankush-asabharwal <ankusha0209.be21@gmail.com>
2024-12-20 11:53:18 +0530

    Adding the files

---
+++ 18Dec,2024/rand.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Welcome to Employee wage computation"<<endl;
+
+    srand(1);
+    for(int i=0;i<3;i++){
+        cout << rand() << endl;
+    }
+
+    // int randomNum = rand()%2;
+    // if(randomNum==1){
+    //     cout << "Employee is present" << endl;
+    // }
+    // else{
+    //     cout << "Employee is not present" << endl;
+    // }
+}
---
+++ 18Dec,2024/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+srand(1);
+    for(int i=0;i<3;i++){
+        cout << rand() << endl;
+    }
